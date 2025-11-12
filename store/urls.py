a86f6b68a1cce363a85f221f7b3f9c8c9ccabdd0
Rainballs <rainballs.niki@gmail.com>
2025-11-12 12:00:55 +0200

    test

--- store/urls.py
+++ store/urls.py
@@ -23,11 +23,11 @@ urlpatterns = [
     path('order/', views.order_info, name='order_info'),
     path('order-summary/<int:pk>/', views.order_summary, name='order_summary'),
     path('card-payment/', TemplateView.as_view(template_name="store/card_payment.html"), name='card_payment'),
-    path('order/<int:order_id>/confirm/', views.confirm_order, name='confirm_order'),
+    # path('order/<int:order_id>/confirm/', views.confirm_order, name='confirm_order'),
 
-    path("shipping/econt/<int:order_id>/", views.econt_redirect, name="econt_redirect"),
+    # path("shipping/econt/<int:order_id>/", views.econt_redirect, name="econt_redirect"),
     # Test
-    path("test-econt/", views.test_econt_label, name="test_econt"),
+    # path("test-econt/", views.test_econt_label, name="test_econt"),
 
     # Use order_summary as the detail view for order confirmation
     # path('order/<int:order_id>/', views.order_summary, name='order_detail'),
